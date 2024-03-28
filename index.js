1a2d99d230a033ac3cdda6c0cd55e13598bcb482
Francisco <francisco.cornejo1@mail.udp.cl>
2024-03-28 03:32:37 -0300

    desafio19_BlackAndWhite resuelto

---
+++ index.js
@@ -0,0 +1,12 @@
+//crear el servidor de node con express
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3000;
+import router from "./routes/router.js";
+
+app.use(express.static("assets"));
+app.use("/", router);
+
+app.listen(port, () => {
+  console.log(`Servidor iniciado en el puerto http://localhost:${port}`);
+});
